78dc5e8de28604b5b1de55645e56c2f41ed05e09
Daniel De La Pena <del17011@byui.edu>
2022-10-12 20:57:03 -0600

    comment routes are protected

--- backend/controllers/comments.js
+++ backend/controllers/comments.js
@@ -26,7 +26,6 @@ const getCommentByUser = (req, res, next) => {
 
 const getCommentByPost = (req, res, next) => {
   const postId = req.params.id;
-  console.log(postId)
   CommentModel.find({ post: postId }, (err, docs) => {
     if (err) {
       const apiError = new ApiError400(err.message);
@@ -40,52 +39,77 @@ const getCommentByPost = (req, res, next) => {
   });
 };
 
-const editComment = (req, res, next) => {
-  const edit = {
-    content: req.body.content,
-  };
-  CommentModel.findByIdAndUpdate(req.body.id, edit, (err, doc) => {
-    if (err) {
-      const apiError = new ApiError400(err.message);
-      next(apiError);
-    } else if (!doc) {
-      const apiError = new ApiError404(err.message);
-      next(apiError);
-    } else {
-      res.status(200).send(doc);
+const editComment = async (req, res, next) => {
+  try {
+    if (!req.accountId) {
+      throw new ApiError401("Unauthorized user.");
     }
-  });
+    const edit = {
+      content: req.body.content,
+    };
+    const commentDoc = await CommentModel.findById(req.params.id);
+    if (!commentDoc) {
+      throw new ApiError404("Comment not found");
+    }
+    if (req.accountId != commentDoc.owner) {
+      throw new ApiError401("Unauthorized user.");
+    }
+    await commentDoc.updateOne(edit);
+    await commentDoc.save();
+    res.status(200).send(commentDoc);
+  } catch (err) {
+    next(err);
+  }
 };
 
-const deleteComment = (req, res, next) => {
-  CommentModel.findByIdAndDelete(req.params.id, (err, doc) => {
-    if (err) {
-      const apiError = new ApiError400(err.message);
-      next(apiError);
-    } else if (!doc) {
-      const apiError = new ApiError404(err.message);
-      next(apiError);
-    } else {
-      res.status(200).send(doc);
+const deleteComment = async (req, res, next) => {
+  try {
+    if (!req.accountId) {
+      throw new ApiError401("Unauthorized user.");
     }
-  });
+    const commentDoc = await CommentModel.findById(req.params.id);
+    if (!commentDoc) {
+      throw new ApiError404("Comment not found.");
+    }
+    if (req.accountId != commentDoc.owner) {
+      throw new ApiError401("Unauthorized user.");
+    }
+
+    commentDoc.deleteOne((err, doc) => {
+      if (err) {
+        const apiError = new ApiError400(err.message);
+        next(apiError);
+      } else {
+        res.status(200).send(doc);
+      }
+    });
+  } catch (err) {
+    next(err);
+  }
 };
 
 const addComment = (req, res, next) => {
-  const comment = {
-    content: req.body.content,
-    owner: req.body.owner,
-    post: req.body.post,
-  };
-
-  CommentModel.create(comment, (err, doc) => {
-    if (err) {
-      const apiError400 = new ApiError400();
-      next(apiError400);
-    } else {
-      res.status(200).send(doc);
+  try {
+    if(!req.accountId){
+      throw new ApiError400("Unauthorized.")
     }
-  });
+    const comment = {
+      content: req.body.content,
+      owner: req.body.owner,
+      post: req.body.post,
+    };
+
+    CommentModel.create(comment, (err, doc) => {
+      if (err) {
+        const apiError400 = new ApiError400();
+        next(apiError400);
+      } else {
+        res.status(200).send(doc);
+      }
+    });
+  } catch (err) {
+    next(err);
+  }
 };
 
 module.exports = {
--- backend/routes/comments.js
+++ backend/routes/comments.js
@@ -1,18 +1,19 @@
-const router = require("express").Router()
-const commentController = require("../controllers/comments")
+const router = require("express").Router();
+const commentController = require("../controllers/comments");
+const { getAuthToken } = require("../middleware/auth");
 
-router.get("/", commentController.getAllComments)
+router.get("/", commentController.getAllComments);
 
-router.get("/getById/:id", commentController.getCommentById)
+router.get("/getById/:id", commentController.getCommentById);
 
-router.get("/getByPost/:id", commentController.getCommentByPost)
+router.get("/getByPost/:id", commentController.getCommentByPost);
 
-router.get("/getByUser/:id", commentController.getCommentByUser)
+router.get("/getByUser/:id", commentController.getCommentByUser);
 
-router.patch("/edit/:id", commentController.editComment)
+router.patch("/edit/:id", getAuthToken, commentController.editComment);
 
-router.delete("/delete/:id", commentController.deleteComment)
+router.delete("/delete/:id", getAuthToken, commentController.deleteComment);
 
-router.post("/add", commentController.addComment)
+router.post("/add", getAuthToken, commentController.addComment);
 
-module.exports = router
+module.exports = router;
