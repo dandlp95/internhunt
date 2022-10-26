7a95dff4ca5441b6ebb90986d882122d78174c99
Daniel De La Pena <del17011@byui.edu>
2022-10-25 22:18:15 -0600

    started working on VotingHistory route and controller

---
+++ backend/controllers/votingHistory.js
@@ -0,0 +1,32 @@
+const VotingHistory = require("../models/votingHistory");
+const ApiError401 = require("../middleware/error-handling/apiError401");
+const ApiError400 = require("../middleware/error-handling/apiError400");
+const ApiError404 = require("../middleware/error-handling/apiError404");
+
+const getVotingHistoryByIds = (req, res, next) => {
+  try {
+    if (!req.accountId) {
+      throw new ApiError401("Not authorized.");
+    }
+    VotingHistory.findOne(
+      { voter: req.accountId, post: req.params.id },
+      (err, doc) => {
+        if (err) {
+          const apiError = new ApiError400(err.message);
+          next(apiError);
+        } else if (!doc) {
+          const apiError = new ApiError404("No doc found");
+          next(apiError)
+        } else {
+          res.status(200).send(doc);
+        }
+      }
+    );
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = {
+    getVotingHistoryByIds
+}
