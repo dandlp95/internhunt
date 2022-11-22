986dfb92b523b8dcf15b3f03f627e2810fa34050
Daniel De La Pena <del17011@byui.edu>
2022-11-21 20:39:50 -0700

    fixed background color in some elements

--- frontend/internhunt/src/pages/createPost.js
+++ frontend/internhunt/src/pages/createPost.js
@@ -7,6 +7,7 @@ import { isAuth } from "../utils/isLoggedIn";
 import { Navigate, useNavigate } from "react-router-dom";
 import getUserById from "../utils/getUserId";
 import Header from "../components/header";
+import "./createPost.css";
 
 /* MajorInput component */
 const MajorInput = (props) => {
@@ -127,126 +128,131 @@ const CreatePost = () => {
       alert("Error!");
     }
   };
-  
+
   if (user) {
     return (
-      <div>
+      <div className="create-post-main">
         <Header accountId={user._id} />
-        <div>
-          <input
-            placeholder="Title"
-            type="text"
-            onChange={(e) => setTitle(e.target.value)}
-          />
-        </div>
-        <div>
-          <input
-            placeholder="Text"
-            type="text"
-            onChange={(e) => setContent(e.target.value)}
-          />
-        </div>
-        <div>
-          <label>For what majors is this post relevant?</label>
-          {majorInputList}
-        </div>
-        <div>
-          <p>What category best describes your post? </p>
-          <div onChange={(e) => setType(e.target.value)}>
-            <input
-              type="radio"
-              value="Question"
-              name="type "
-              checked={type === "Question"}
-            />{" "}
-            Question
-            <input
-              type="radio"
-              value="Review"
-              name="type"
-              checked={type === "Review"}
-            />
-            Internship review
-            <input
-              type="radio"
-              value="Advise"
-              name="type"
-              checked={type === "Advise"}
-            />
-            Advise
-          </div>
-        </div>
-        <div>
-          <section>
+        <div className="create-post-page">
+          <div className="grid-column-one"></div>
+          <div className="grid-column-two">
+            <h3>Create a post</h3>
             <div>
-              <h3>If applicable, fill the following information.</h3>
               <input
-                placeholder="Company name"
+                placeholder="Title"
                 type="text"
-                onChange={(e) => setCompany(e.target.value)}
+                onChange={(e) => setTitle(e.target.value)}
               />
             </div>
             <div>
-              <label>State</label>
-              <select onChange={handleChange}>
-                <option value="">--Select a state--</option>
-                <option value="AL">Alabama</option>
-                <option value="AK">Alaska</option>
-                <option value="AZ">Arizona</option>
-                <option value="AR">Arkansas</option>
-                <option value="CA">California</option>
-                <option value="CO">Colorado</option>
-                <option value="CT">Connecticut</option>
-                <option value="DE">Delaware</option>
-                <option value="DC">District Of Columbia</option>
-                <option value="FL">Florida</option>
-                <option value="GA">Georgia</option>
-                <option value="HI">Hawaii</option>
-                <option value="ID">Idaho</option>
-                <option value="IL">Illinois</option>
-                <option value="IN">Indiana</option>
-                <option value="IA">Iowa</option>
-                <option value="KS">Kansas</option>
-                <option value="KY">Kentucky</option>
-                <option value="LA">Louisiana</option>
-                <option value="ME">Maine</option>
-                <option value="MD">Maryland</option>
-                <option value="MA">Massachusetts</option>
-                <option value="MI">Michigan</option>
-                <option value="MN">Minnesota</option>
-                <option value="MS">Mississippi</option>
-                <option value="MO">Missouri</option>
-                <option value="MT">Montana</option>
-                <option value="NE">Nebraska</option>
-                <option value="NV">Nevada</option>
-                <option value="NH">New Hampshire</option>
-                <option value="NJ">New Jersey</option>
-                <option value="NM">New Mexico</option>
-                <option value="NY">New York</option>
-                <option value="NC">North Carolina</option>
-                <option value="ND">North Dakota</option>
-                <option value="OH">Ohio</option>
-                <option value="OK">Oklahoma</option>
-                <option value="OR">Oregon</option>
-                <option value="PA">Pennsylvania</option>
-                <option value="RI">Rhode Island</option>
-                <option value="SC">South Carolina</option>
-                <option value="SD">South Dakota</option>
-                <option value="TN">Tennessee</option>
-                <option value="TX">Texas</option>
-                <option value="UT">Utah</option>
-                <option value="VT">Vermont</option>
-                <option value="VA">Virginia</option>
-                <option value="WA">Washington</option>
-                <option value="WV">West Virginia</option>
-                <option value="WI">Wisconsin</option>
-                <option value="WY">Wyoming</option>
-              </select>
+              <input
+                placeholder="Text"
+                type="text"
+                onChange={(e) => setContent(e.target.value)}
+              />
             </div>
-          </section>
-        </div>
-        <div>
-          <Button text="Post" action={postPost} />
+            <div>
+              <label>For what majors is this post relevant?</label>
+              {majorInputList}
+            </div>
+            <div>
+              <p>What category best describes your post? </p>
+              <div onChange={(e) => setType(e.target.value)}>
+                <input
+                  type="radio"
+                  value="Question"
+                  name="type "
+                  checked={type === "Question"}
+                />{" "}
+                Question
+                <input
+                  type="radio"
+                  value="Review"
+                  name="type"
+                  checked={type === "Review"}
+                />
+                Internship review
+                <input
+                  type="radio"
+                  value="Advise"
+                  name="type"
+                  checked={type === "Advise"}
+                />
+                Advise
+              </div>
+            </div>
+            <div>
+              <section>
+                <div>
+                  <h3>If applicable, fill the following information.</h3>
+                  <input
+                    placeholder="Company name"
+                    type="text"
+                    onChange={(e) => setCompany(e.target.value)}
+                  />
+                </div>
+                <div>
+                  <label>State</label>
+                  <select onChange={handleChange}>
+                    <option value="">--Select a state--</option>
+                    <option value="AL">Alabama</option>
+                    <option value="AK">Alaska</option>
+                    <option value="AZ">Arizona</option>
+                    <option value="AR">Arkansas</option>
+                    <option value="CA">California</option>
+                    <option value="CO">Colorado</option>
+                    <option value="CT">Connecticut</option>
+                    <option value="DE">Delaware</option>
+                    <option value="DC">District Of Columbia</option>
+                    <option value="FL">Florida</option>
+                    <option value="GA">Georgia</option>
+                    <option value="HI">Hawaii</option>
+                    <option value="ID">Idaho</option>
+                    <option value="IL">Illinois</option>
+                    <option value="IN">Indiana</option>
+                    <option value="IA">Iowa</option>
+                    <option value="KS">Kansas</option>
+                    <option value="KY">Kentucky</option>
+                    <option value="LA">Louisiana</option>
+                    <option value="ME">Maine</option>
+                    <option value="MD">Maryland</option>
+                    <option value="MA">Massachusetts</option>
+                    <option value="MI">Michigan</option>
+                    <option value="MN">Minnesota</option>
+                    <option value="MS">Mississippi</option>
+                    <option value="MO">Missouri</option>
+                    <option value="MT">Montana</option>
+                    <option value="NE">Nebraska</option>
+                    <option value="NV">Nevada</option>
+                    <option value="NH">New Hampshire</option>
+                    <option value="NJ">New Jersey</option>
+                    <option value="NM">New Mexico</option>
+                    <option value="NY">New York</option>
+                    <option value="NC">North Carolina</option>
+                    <option value="ND">North Dakota</option>
+                    <option value="OH">Ohio</option>
+                    <option value="OK">Oklahoma</option>
+                    <option value="OR">Oregon</option>
+                    <option value="PA">Pennsylvania</option>
+                    <option value="RI">Rhode Island</option>
+                    <option value="SC">South Carolina</option>
+                    <option value="SD">South Dakota</option>
+                    <option value="TN">Tennessee</option>
+                    <option value="TX">Texas</option>
+                    <option value="UT">Utah</option>
+                    <option value="VT">Vermont</option>
+                    <option value="VA">Virginia</option>
+                    <option value="WA">Washington</option>
+                    <option value="WV">West Virginia</option>
+                    <option value="WI">Wisconsin</option>
+                    <option value="WY">Wyoming</option>
+                  </select>
+                </div>
+              </section>
+            </div>
+            <Button text="Post" action={postPost} />
+          </div>
+          <div className="grid-column-three"></div>
         </div>
       </div>
     );
