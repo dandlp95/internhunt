080e58ae9e42c5f04b145ca60b2f5dc47d07eb0c
Daniel De La Pena <del17011@byui.edu>
2022-11-27 20:39:45 -0700

    css in about page

--- frontend/internhunt/src/pages/aboutPage.js
+++ frontend/internhunt/src/pages/aboutPage.js
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import Header from "../components/header";
 import { isAuth } from "../utils/isLoggedIn";
 import about from "../utils/about.json";
+import "./aboutPage.css#"
 
 const AboutPage = () => {
   const [user, setUser] = useState([]);
@@ -26,7 +27,7 @@ const AboutPage = () => {
   return (
     <div>
       <Header accountId={user} />
-      <div className="aboutPage">
+      <div className="about-page">
         <h2>{about.title}</h2>
         <p>{about.paragraph}</p>
         <p>{about.paragraph2}</p>
